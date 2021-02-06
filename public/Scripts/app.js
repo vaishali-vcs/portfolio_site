bf502342af93ddde33492e249a693bf9d4bf6c87
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-02-06 13:09:31 -0500

    skeleton of the ExpressJS site with only 5 routes.
    Installed backend dependencies - EJS-Generator, morgan, ,
    Installed all front end dependencies - Bootstrap frontend, jquery, popper.js, fontawesome

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+// IIFE Immediately Invoked Function
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+module.exports = router;
